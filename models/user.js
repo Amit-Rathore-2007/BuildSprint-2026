e925444ba0c9e77be6323200fc895862ec449abd
Amit Kumar Rathore <rathoreamit2007@gmail.com>
2026-06-28 00:52:28 +0530

    Refactor user schema to include only email field
    Removed multiple fields from user schema and added email field.

--- models/user.js
+++ models/user.js
@@ -3,39 +3,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-    contact: {
-        type: Number,
-        required: true,
-    },
-    coll_roll_no: {
-        type: Number,
-        required: true,
-    },
-    coll_branch: {
-        type: String,
-        required: true
-    },
-    coll_joined_year : {
-        type: Date,
-        required: true
-    },
-    coll_email: {
+    email: {
         type: String,
         required: true,
         set: v => v.toLowerCase(),
