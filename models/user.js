a7ff248d2a49b553b166d00dc641f2e40ffa41cc
Amit Kumar Rathore <rathoreamit2007@gmail.com>
2026-06-28 00:54:43 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose").default;
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        set: v => v.toLowerCase(),
-    },
+    email: string,
 })
 
 userSchema.plugin(passportLocalMongoose);
